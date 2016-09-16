83473efcb517bc44340ea603530f60b12ae0691e
juniorabranches <jr-abranches@hotmail.com>
2016-09-15 22:29:35 -0400

    Revert "Add files via upload"
    This reverts commit a53e681c5887c71fe775fdb8cb04b5ee72ce339c.

--- CustomData.m
+++
@@ -1,9 +0,0 @@
-//
-//  CustomData.m
-//  Cuiabanidade
-//
-//  Created by Junior Abranches on 30/12/15.
-//  Copyright © 2015 Junior Abranches. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
